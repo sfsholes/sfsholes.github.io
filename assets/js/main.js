ee9fb537dbc3629b212e1e9d16aea7347abc4191
sfsholes <steven.f.sholes@gmail.com>
2021-09-01 22:25:35 -0700

    Fixed image links

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '.work-item a.pizza',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
